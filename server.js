b9978799a3aeb2eded6af0a5c209d7af931415c0
Akshat Mittal <akshatmittal2506@gmail.com>
2024-04-16 17:05:11 +0530

    configured static assets

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from "express";
 import { PORT } from "./config/index.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
 	console.info(`Server is listening at http://localhost:${PORT}`);
